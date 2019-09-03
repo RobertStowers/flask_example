86f214904a115a05fa68b6ffc9d703dad6e7978f
Robert Stowers <robert.stowers@gmail.com>
2019-09-02 23:18:43 -0400

    Modifed main.py

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ def home():
     return render_template("home.html")
 
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
 @app.route("/salvador")
 def salvador():
     return "Hello, Salvador"
